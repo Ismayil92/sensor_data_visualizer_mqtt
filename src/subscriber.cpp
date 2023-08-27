c8f316db654503d9c74dedc0e6efa8f17daf170b
Ismayil Ahmadli <ismayil_ahmedli@hotmail.com>
2023-08-27 18:16:29 +0200

    shaders added

--- src/subscriber.cpp
+++ src/subscriber.cpp
@@ -5,10 +5,10 @@
 #include <vector>
 #include <cstdlib>
 #include <future>
-#include "mqtt/client.h"
-#include "mqtt/topic.h"
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include "mqtt/client.h"
+#include "mqtt/topic.h"
 
 
 
@@ -17,6 +17,8 @@ using namespace std::chrono_literals;
 
 const std::string SERVER_ADDRESS{"tcp://localhost:1883"};
 const std::string CLIENT_ID{"sensor_listener"};
+const std::string TOPIC_NAME{"coords"};
+const int QOS{0};
 
 static void error_callback(int error, const char* description)
 {
@@ -51,8 +53,6 @@ void listen()
 
 void setupMQTT()
 {
-    const std::string TOPIC_NAME{"coords"};
-    const int QOS{0};
 
     std::cout<<"MQTT Subscriber Client connecting to the MQTT server!!!\n";
     mqtt::client cli(SERVER_ADDRESS, CLIENT_ID, 
@@ -114,9 +114,73 @@ void setupMQTT()
 }
 
 
+bool setVertexShader(uint& vertex_shader)
+{
+    int success;
+    char infoLog[512];
+    const char* vertexShaderSource = "#version 460 core\n"
+    "layout(location=0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader,1,&vertexShaderSource, NULL);
+    //shader source code must compile dynamically in run-time
+    glCompileShader(vertex_shader);
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(vertex_shader, 512, NULL, infoLog);
+        std::cout<<"ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"<<infoLog<<std::endl;
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+
+bool setFragmentShader(uint& fragment_shader)
+{
+    int success;
+    char infoLog[512];
+    const char* fragmentShaderSource = "#version 330 core\n"
+                                "out vec4 FragColor;\n"
+                                "void main()\n"
+                                "{\n"
+                                "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                "}\0";
+
+    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragment_shader);
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(fragment_shader, 512, NULL, infoLog);
+        std::cout<<"ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"<<infoLog<<std::endl;
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
 int main(int argc, char** argv)
 {
+ 
 
+    GLFWwindow *window;  
+    uint vertex_shader;
+    uint fragment_shader;
+    uint shader_program;
+    uint VBO; //vertex buffer object  
+    uint VAO; //vertex array object 
+    //declare a future object related to MQTT communication
     std::future<void> async_thr_mqtt_receiver{std::async(std::launch::async,
                     setupMQTT)};
 
@@ -133,9 +197,7 @@ int main(int argc, char** argv)
     else
     {
         std::cout<<"GL initialized successfully!!!\n";
-    }
-    
-    GLFWwindow *window;
+    }  
    
     glfwSetErrorCallback(error_callback);
     
@@ -146,6 +208,7 @@ int main(int argc, char** argv)
         glfwTerminate();
         std::exit(EXIT_FAILURE);
     }
+    
     //create context of the rendering window in the main thread
     glfwMakeContextCurrent(window);
     //load all HW and system specific GL functions
@@ -160,32 +223,74 @@ int main(int argc, char** argv)
     //wrt the window. 
     glViewport(0,0, 640, 800);
     //mount callback function to the event of window resize
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);    
+    glfwSwapInterval(1);
+
+    //configure and compile shaders
+    //in modern OpenGL we are required to define at least  a custom  vertex and fragment shader.
+    bool ver_shader_res{setVertexShader(vertex_shader)};
+    bool frag_shader_res{setFragmentShader(fragment_shader)};
+    //create a shader program object 
+    shader_program = glCreateProgram();
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+    glLinkProgram(shader_program);
+    int shader_program_success;
+    char infoLog[512];
+    glGetProgramiv(shader_program,GL_LINK_STATUS, &shader_program_success);
+    if(!shader_program_success)
+    {
+        glGetProgramInfoLog(shader_program, 512, NULL, infoLog);
+        std::cerr<<"The program could not link to the shaders!!!\n";
+        std::exit(EXIT_FAILURE);
+    }   
+
+    float vertices[] = {
+            -0.5f, -0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            0.0f,  0.5f, 0.0f
+        }; 
+    
 
     
-    glfwSwapInterval(1);
+    //generate VAO object 
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    glEnableVertexAttribArray(0);
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    //save vertices in gpu memory
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); 
+    //we have to specify how OpenGL should interpret vertex data
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);    
+
+    
     //render loop
     while (!glfwWindowShouldClose(window))
     {
         //check the input key at each iteration
         processInput(window);
-
         //rendering commands 
         //at each frame cycle, we need to clear the screen otherwise old colors
         //from old frame will still hold on the viewport
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         //we specify which buffer we would like to clear
         glClear(GL_COLOR_BUFFER_BIT);
-
+        //now we are activating newly created program object 
+        glUseProgram(shader_program); 
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);     
         
+           
         //rendering is shown on the display
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-   
-
-    
-
+       
+    //now we can delete shader objects after linking them to program object
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+    glDeleteVertexArrays(1, &VAO);
     glfwTerminate();
     return 0;
 
